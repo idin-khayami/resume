78336a53a93942ccf8d03e0e36d1770fd70bd77f
idin.khayami <idin.khayami@snapp.cab>
2019-07-20 13:52:18 +0430

    Initial dist subtree commit

---
+++ dist/precache-manifest.1cd4b3607c00c50ec6715a3c17ad8553.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c33a78384f1b37211187a24ea9663cdf",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "7cca60461b76fa15f568",
+    "url": "/resume/static/css/main.9f2e87bb.chunk.css"
+  },
+  {
+    "revision": "60f54d0cd278b4dd46ac",
+    "url": "/resume/static/js/2.a69ababd.chunk.js"
+  },
+  {
+    "revision": "7cca60461b76fa15f568",
+    "url": "/resume/static/js/main.f067d9eb.chunk.js"
+  },
+  {
+    "revision": "699f89d957187d0ac949",
+    "url": "/resume/static/js/runtime~main.30522d0f.js"
+  },
+  {
+    "revision": "557025919b54a31e81ae82037c8374ec",
+    "url": "/resume/static/media/letter-background.55702591.jpg"
+  }
+]);
