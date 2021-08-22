53b47a799cea41ebe81767f1c7ee5220ce6aa9f1
Idin Khayami <idin.khanoom.khayami@gmail.com>
2021-08-22 23:59:08 +0430

    update gh-pages

---
+++ precache-manifest.51d38ef2755d7c2cf4860d76251c001e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e57d94e1a45716ea3d865078fb83b14c",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "778205aff89624a8221f",
+    "url": "/resume/static/css/main.9f2e87bb.chunk.css"
+  },
+  {
+    "revision": "441922f01369af314479",
+    "url": "/resume/static/js/2.fe43ec02.chunk.js"
+  },
+  {
+    "revision": "64c44d1cefc3299cbd4358bd0ccbe7e6",
+    "url": "/resume/static/js/2.fe43ec02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "778205aff89624a8221f",
+    "url": "/resume/static/js/main.8f5fe5a3.chunk.js"
+  },
+  {
+    "revision": "8c1d914a1677cea53979",
+    "url": "/resume/static/js/runtime-main.75d8c91e.js"
+  },
+  {
+    "revision": "557025919b54a31e81ae82037c8374ec",
+    "url": "/resume/static/media/letter-background.55702591.jpg"
+  }
+]);
